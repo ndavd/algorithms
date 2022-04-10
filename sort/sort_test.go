7d19e9f6d8f4edf5825277ecbef018eeca7ff5b5
Nuno David <email@ndavd.com>
2022-04-10 16:06:19 +0100

    Add benchmark testing

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,8 +1,12 @@
 package sort
 
 import (
+  "math/rand"
   "testing"
+  "time"
 )
+
+/* UNIT TESTING */
 
 // Check slice equality
 func areEqual[T Ordered](a []T, b []T) bool {
@@ -53,4 +57,50 @@ func TestMergesort(t *testing.T) {
 
 func TestQuicksort(t *testing.T) {
   testAlgorithm(t, Quicksort[int], "Quicksort")
+}
+
+/* BENCHMARK TESTING */
+
+// Check if slice is sorted
+func isSorted[T Ordered](a []T) bool {
+  for i := 0; i < len(a)-1; i++ {
+    if a[i + 1] < a[i] {
+      return false
+    }
+  }
+  return true
+}
+
+func benchmarkAlgorithm(b *testing.B, a func([]int), name string) {
+  for i := 0; i < b.N; i++ {
+    var test []int
+    rand.Seed(time.Now().Unix())
+    for i := 0; i < 10000; i++ {
+      test = append(test, rand.Intn(200) - 100)
+    }
+    a(test)
+    if !isSorted(test) {
+      b.Errorf("%v() FAILED. Not properly sorted!\n", name)
+    }
+  }
+}
+
+func BenchmarkSelection(b *testing.B) {
+  benchmarkAlgorithm(b, Selection[int], "Selection")
+}
+
+func BenchmarkInsertion(b *testing.B) {
+  benchmarkAlgorithm(b, Insertion[int], "Insertion")
+}
+
+func BenchmarkBubble(b *testing.B) {
+  benchmarkAlgorithm(b, Bubble[int], "Bubble")
+}
+
+func BenchmarkMergesort(b *testing.B) {
+  benchmarkAlgorithm(b, Mergesort[int], "Mergesort")
+}
+
+func BenchmarkQuicksort(b *testing.B) {
+  benchmarkAlgorithm(b, Quicksort[int], "Quicksort")
 }
